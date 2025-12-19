e7d7a44c9f29f510d4e1a13251d60a0a320f2fc6
Ramu Kumar <rk9120541@gmail.com>
2025-12-19 17:14:48 +0530

    updated app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":
 
 llm=HuggingFaceEndpoint(
     repo_id='deepseek-ai/DeepSeek-V3.2',
-    task='text-generation'
+    task='text-generation',
+    base_url="https://router.huggingface.co"
 )
 model=ChatHuggingFace(llm=llm)
 prompt = ChatPromptTemplate.from_messages(
